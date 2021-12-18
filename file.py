de42155c17a96739eb1f801038dfd26912b96601
rajdips834 <rajdips834@gmail.com>
2021-12-18 11:17:35 +0530

    Init

--- file.py
+++ file.py
@@ -1,40 +1,65 @@
-file1 = open("myfile.txt", "w")
-L = ["This is Delhi \n", "This is Paris \n", "This is London \n"]
+# file1 = open("myfile.txt", "w")
+# L = ["This is Delhi \n", "This is Paris \n", "This is London \n"]
+#
+# file1.write("Hello \n")
+# file1.writelines(L)
+# file1.close()
+#
+# file1 = open("myfile.txt", "r+")
+#
+# print("Output of Read function is ")
+# print(file1.read())
+# print()
+#
+# file1.seek(0)
+#
+# print("Output of Readline function is ")
+# prin(file1.readline())
+# print()
+#
+# file1.seek(0)
+#
+#
+# print("Output of Read(9) function is ")
+# print(file1.read(9))
+# print()
+#
+# file1.seek(0)
+#
+# print("Output of Readline(9) function is ")
+# print(file1.readline(9))
+# print()
+#
+# file1.seek(0)
+#
+#
+#
+# print("Output of Readlines function is ")
+# print(file1.readlines())
+# print()
+# file1.close()
+# f3=open("C://Users//rajdi//Downloads//myfile.txt",'r')
+# print(f3)
+# s,a=input('Enter Sentence'),''
+# for i in s:
+#     if i.islower()==True:
+#         a=a+ i.upper()
+#     else:
+#
+#         a=a + i.lower()
+# print(a)
+# s=input('Enter a sentence')
+#  a=s[::-1]
+#  print(a)
+# b=s[::-1]
+# print(b)
+# s=input('Enter a sentence ')
+# s.swapcase()
+# print(s)
+s='Lorem ipsum dolor sit amet'
+str=s.partition('ip')
+print(str)
 
-file1.write("Hello \n")
-file1.writelines(L)
-file1.close()
+print(s.title())
+print(s.count('m'))
 
-file1 = open("myfile.txt", "r+")
-
-print("Output of Read function is ")
-print(file1.read())
-print()
-
-file1.seek(0)
-
-print("Output of Readline function is ")
-prin(file1.readline())
-print()
-
-file1.seek(0)
-
-
-print("Output of Read(9) function is ")
-print(file1.read(9))
-print()
-
-file1.seek(0)
-
-print("Output of Readline(9) function is ")
-print(file1.readline(9))
-print()
-
-file1.seek(0)
-
-
-
-print("Output of Readlines function is ")
-print(file1.readlines())
-print()
-file1.close()
